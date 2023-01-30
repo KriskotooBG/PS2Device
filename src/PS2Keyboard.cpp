c33b442a18eda6f6c46783f79a0af39ae6697d0e
KriskotooBG <29774713+KriskotooBG@users.noreply.github.com>
2023-01-30 01:31:24 +0100

    Added comment headers

--- src/PS2Keyboard.cpp
+++ src/PS2Keyboard.cpp
@@ -1,6 +1,6 @@
 /**
- * PS2Device.cpp
- * 
- * 
+ * PS2Keyboard.cpp
+ * This class takes is used to emulate a PS2 Keyboard device to the host device.
+ * Author: Kris G.
  * 
 */
---
+++ src/PS2Keyboard.h
@@ -0,0 +1,6 @@
+/**
+ * PS2Keyboard.h
+ * Header file for PS2Keyboard.cpp
+ * Author: Kris G.
+ * 
+*/
